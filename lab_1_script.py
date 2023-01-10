5093be9df296ca3a293667d3fe486f2b52216178
jacob-gerun <77089997+jacob-gerun@users.noreply.github.com>
2023-01-09 18:24:19 -0700

    commit 2

--- lab_1_script.py
+++ lab_1_script.py
@@ -1,5 +1,5 @@
 from urllib import request
 import requests
-print("Hello")
 print(requests.__version__)
 print(requests.get("http://www.google.com"))
+print(requests.get("https://raw.githubusercontent.com/jacob-gerun/CMPUT_404_Lab_1/main/lab_1_script.py").text)
